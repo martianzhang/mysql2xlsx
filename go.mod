c46e61f6fcd9556138cf96aae25a996eaa6999da
Leon Zhang <martianzhang@hotmail.com>
2020-08-27 23:24:45 +0800

    add go.mod and restruct code

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/martianzhang/mysql2xlsx
+
+go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c
+	github.com/tealeg/xlsx v1.0.5
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6 // indirect
+)
--- mysql2xlsx.go
+++ mysql2xlsx.go
@@ -6,122 +6,166 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/howeyc/gopass"
 	"github.com/tealeg/xlsx"
 )
 
-var mysqlHost, mysqlPort, mysqlUser, mysqlPassword, mysqlDb *string
-var excelFilePath *string
-var db *sql.DB
+type config struct {
+	// MySQL config
+	User     string
+	Host     string
+	Port     string
+	Password string
+	Database string
+	Charset  string
 
-func main() {
-	mysqlHost = flag.String("h", "localhost", "mysql host")
-	mysqlUser = flag.String("u", "", "mysql user name")
-	mysqlPassword = flag.String("p", "", "mysql password")
-	mysqlDb = flag.String("d", "", "mysql database name")
-	mysqlPort = flag.String("P", "3306", "mysql port")
-	excelFilePath = flag.String("t", "/tmp/databse.xlsx", "export xlsx path and filename")
+	// other config
+	Query string // select query
+	File  string // storage file abs path
+}
 
+var cfg config
+
+func parseFlag() error {
+	mysqlHost := flag.String("h", "localhost", "mysql host")
+	mysqlUser := flag.String("u", "", "mysql user name")
+	mysqlPassword := flag.String("p", "", "mysql password")
+	mysqlDatabase := flag.String("d", "", "mysql database name")
+	mysqlPort := flag.String("P", "3306", "mysql port")
+	mysqlCharset := flag.String("c", "utf8mb4", "mysql default charset")
+	mysqlQuery := flag.String("q", "", "select query")
+	fileName := flag.String("f", "", "xlsx file name")
 	flag.Parse()
 
-	if *mysqlHost == "" || *mysqlDb == "" || *excelFilePath == "" || *mysqlUser == "" {
-		flag.PrintDefaults()
-		return
+	if *mysqlPassword == "" {
+		fmt.Print("Password:")
+		password, err := gopass.GetPasswd()
+		if err != nil {
+			return err
+		}
+		*mysqlPassword = strings.TrimSpace(string(password))
 	}
 
-	excelAbsFilePath, err := filepath.Abs(*excelFilePath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Printf("Enter database password:\n")
-	*mysqlPassword, _ = reader.ReadString('\n')
-	*mysqlPassword = strings.TrimSpace(*mysqlPassword)
-
-	//获取Sql语句
-	var sqlStr string
-	fmt.Printf("Please Input SQL:\n")
-	for {
-		tmpSql, _ := reader.ReadString('\n')
-		sqlStr = sqlStr + tmpSql
-		tmpSql = strings.TrimSpace(tmpSql)
-		if tmpSql[len(tmpSql)-1] == ';' {
-			break
+	if *mysqlQuery == "" {
+		// allow line separator, sql end with ';'
+		fmt.Println("Query (end with '; + <Enter>'):")
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			tmpSql, err := reader.ReadString('\n')
+			if err != nil {
+				return err
+			}
+			*mysqlQuery = *mysqlQuery + tmpSql
+			tmpSql = strings.TrimSpace(tmpSql)
+			if tmpSql[len(tmpSql)-1] == ';' {
+				break
+			}
 		}
 	}
 
-	//获取sql连接
-	dsn := *mysqlUser + ":" + *mysqlPassword + "@tcp(" + *mysqlHost + ":" + *mysqlPort + ")/" + *mysqlDb + "?charset=utf8"
-	db, err := sql.Open("mysql", dsn)
+	pwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	//查询获取结果
-	stmt, err := db.Prepare(sqlStr)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if *fileName == "" {
+		*fileName = pwd + "/temp.xlsx"
 	}
 
-	defer stmt.Close()
-
-	result, err := stmt.Query()
-	if err != nil {
-		fmt.Println(err)
-		return
+	if !strings.HasPrefix(*fileName, "/") {
+		*fileName = pwd + "/" + *fileName
 	}
 
-	//保存excel
-	saveExcelByRows(excelAbsFilePath, result)
+	cfg = config{
+		User:     *mysqlUser,
+		Host:     *mysqlHost,
+		Port:     *mysqlPort,
+		Password: *mysqlPassword,
+		Database: *mysqlDatabase,
+		Charset:  *mysqlCharset,
+
+		Query: *mysqlQuery,
+		File:  *fileName,
+	}
+
+	return nil
 }
 
-func saveExcelByRows(excelAbsFilePath string, rows *sql.Rows) error {
+func getData() (*sql.Rows, error) {
+	var res *sql.Rows
+
+	// init database connection
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database, cfg.Charset)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return res, err
+	}
+	defer db.Close()
+
+	res, err = db.Query(cfg.Query)
+	return res, err
+}
+
+func save2xlsx(filePath string, rows *sql.Rows) error {
 	file := xlsx.NewFile()
+	// create new sheet
 	sheet, err := file.AddSheet("result")
 	if err != nil {
 		return err
 	}
 
+	// set table header with column name
 	columns, err := rows.Columns()
 	if err != nil {
 		return err
 	}
-
-	//写column数据
-	columnRow := sheet.AddRow()
-	columnLen := len(columns)
+	sheetHeader := sheet.AddRow()
 	for _, name := range columns {
-		cell := columnRow.AddCell()
+		cell := sheetHeader.AddCell()
 		cell.Value = name
 	}
 
-	scanArgs := make([]interface{}, columnLen)
-	values := make([][]byte, columnLen)
+	// set every table rows
+	scanArgs := make([]interface{}, len(columns))
+	values := make([][]byte, len(columns))
 	for i := range values {
 		scanArgs[i] = &values[i]
 	}
 	for rows.Next() {
 		rows.Scan(scanArgs...)
-		row := sheet.AddRow()
+		sheetRow := sheet.AddRow()
 		for _, v := range values {
-			cell := row.AddCell()
+			cell := sheetRow.AddCell()
 			cell.Value = string(v)
 		}
 	}
 
-	//保存文件
-	err = file.Save(excelAbsFilePath)
-	if err != nil {
-		return err
-	} else {
-		return nil
+	// save to file
+	return file.Save(filePath)
+}
+
+func main() {
+	// parse config
+	if err := parseFlag(); err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 
+	// execute sql and get all result rows
+	rows, err := getData()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// save result into xlsx files
+	err = save2xlsx(cfg.File, rows)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("save data into file: '%s'\n", cfg.File)
 }
